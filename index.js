f25cda66f2df3e893ceb96053364157e1e2f6246
Andrew Galkin <andrushka@MacBook-Air-Andrew.local>
2021-08-04 12:37:43 +0300

    add menu

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
+import menuIngredientsList from './templates/menu_ingredients.hbs'
+import ingredients from './menu.json'
+
+
+const menuList = document.querySelector(".js-menu")
+const ingredientsList = createIngredientsList(ingredients);
+menuList.insertAdjacentHTML("beforeend", ingredientsList);
+
+function createIngredientsList(ingredients) {
+    return ingredients.map(menuIngredientsList).join("")
+}
+
 const refs = {
     toggleBtn: document.getElementById("theme-switch-toggle"),
     bodyColor: document.querySelector("body")
 }
-
 const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
 };
+
 const changeThemeSwitch = (event) => {
     if (event.target.checked) {
         refs.bodyColor.classList.remove(Theme.LIGHT)
         refs.bodyColor.classList.add(Theme.DARK)
+
     } else {
         refs.bodyColor.classList.remove(Theme.DARK)
         refs.bodyColor.classList.add(Theme.LIGHT)
     }
 }
-
 refs.toggleBtn.addEventListener("change", changeThemeSwitch);
+
+
+
+
+
+
+
+
+
+
 
 
 
